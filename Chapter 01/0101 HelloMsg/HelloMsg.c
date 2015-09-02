29d7d61e48cc0d371067e5fcd717b8ba424b3206
recombinant <gituser@recombinant.me.uk>
2015-09-02 21:55:44 +0100

    0101 HelloMsg UNREFERENCED_PARAMETER added appropriately. There are no long pointers. Warning level is 4. *.props removed from .gitignore (should not have been there).

--- Chapter 01/0101 HelloMsg/HelloMsg.c
+++ Chapter 01/0101 HelloMsg/HelloMsg.c
@@ -9,9 +9,14 @@
 int WINAPI _tWinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ PTSTR lpCmdLine,
+	_In_ PTSTR pCmdLine,
 	_In_ int nShowCmd)
 {
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(pCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
         // MessageBox() can only return IDOK when using MB_OK
 
     MessageBox(NULL, TEXT("Hello, Windows 98!"), TEXT("HelloMsg"), MB_OK);
